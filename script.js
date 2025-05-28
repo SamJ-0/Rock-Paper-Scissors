ca601c4b909e8e2efbaa878319500ef16af63ad9
SamJ-0 <210594751+SamJ-0@users.noreply.github.com>
2025-05-28 16:28:28 +0100

    Added computer choice function

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ determine winner
 
 */
 
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 4);
+
+    if (computerChoice === 0) {
+        return "Rock";
+    } else if (computerChoice === 1) {
+        return "Paper";
+    } else {
+        return "Scissors"
+    }
+
+}
+
+console.log(getComputerChoice());
+
