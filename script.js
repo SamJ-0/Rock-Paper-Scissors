33d8117df5b719be37460ac8584eec982b237e09
SamJ-0 <210594751+SamJ-0@users.noreply.github.com>
2025-05-29 16:09:39 +0100

    Added a play game function so the game repeats for 5 rounds

--- script.js
+++ script.js
@@ -57,6 +57,14 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
+function playGame() {
+
+for(let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+}
+
+}
+
+playGame();
